ba4a7c4e3066a3f8a1d70c1d74bad685f5ae8874
Getvion <gog0125@mail.ru>
2021-12-16 01:17:50 +0300

    add filter function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import List from './Components/List';
+
+import './index.scss';
 
 function App() {
+  // TODO по энтеру - вбивать вариант из списка, на стрелки менять выбранный элемент
+
+  const [countries, setCountries] = useState([]);
+  const [visibleList, setVisibleList] = useState(false);
+  const [searchValue, setSearchValue] = useState('');
+
+  const getCountries = () => {
+    axios.get('https://restcountries.com/v3.1/all').then((response) => {
+      return setCountries(response.data);
+    });
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
+  const hideList = (e) => {
+    if (e.target.classList.contains('app')) {
+      setVisibleList(false);
+    }
+  };
+
+  const onChangeSearchInput = (e) => {
+    setSearchValue(e.target.value);
+    console.log(filteredCountries);
+  };
+
+  const filteredCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(searchValue.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' onClick={hideList}>
+      <input
+        className='input'
+        type='text'
+        placeholder='Search the country'
+        value={searchValue}
+        onChange={onChangeSearchInput}
+        onClick={() => setVisibleList(true)}
+      />
+      <div className={visibleList ? 'list' : 'list invisible'}>
+        {filteredCountries.map((country, index) => {
+          return <List country={country} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
